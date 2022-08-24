5c4ce4affe8e3146d659cc1d3c3058e0bde614d9
Jumper0 <72196927+Jumper0@users.noreply.github.com>
2022-08-24 09:43:57 +0200

    #03 Add logger in CalculatorDemo.java and create log4j2.xml file

--- src/main/java/de/msgdavid/CalculatorDemo.java
+++ src/main/java/de/msgdavid/CalculatorDemo.java
@@ -1,11 +1,23 @@
 package de.msgdavid;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CalculatorDemo {
+
+	private static final Logger logger = LogManager.getLogger(CalculatorDemo.class);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Calculator calc = new Calculator();
 		System.out.println(calc.add(5, 3));
+
+		logger.trace("We've just greeted the user!");
+		logger.debug("We've just greeted the user!");
+		logger.info("We've just greeted the user!");
+		logger.warn("We've just greeted the user!");
+		logger.error("We've just greeted the user!");
+		logger.fatal("We've just greeted the user!");
 	}
 
 }
