5f62d2bd1b26119b607111a67b5d8479702e031d
Jumper0 <72196927+Jumper0@users.noreply.github.com>
2022-08-24 13:20:33 +0200

    #03 Run some tests and co.

--- src/test/java/de/msgdavid/CalculatorTest.java
+++ src/test/java/de/msgdavid/CalculatorTest.java
@@ -1,7 +1,10 @@
 package de.msgdavid;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -10,54 +13,71 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
+	private static final Logger logger = LogManager.getLogger(CalculatorDemo.class);
+
+	private static Calculator calc;
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		calc = new Calculator();
+		System.out.println("setUpBeforeClass");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		System.out.println("tearDownAfterClass");
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("setUp");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("tearDown");
 	}
 
 	@Test
 	public void testAdd() {
-		Assert.assertEquals(20, Calculator.add(8, 12), 0);
+		Assert.assertEquals(8.0, calc.add(3, 5), 0.0);
+		logger.info("Test 'Add' erfolgreich!");
 	}
 
 	@Test
 	public void testSub() {
-		fail("Not yet implemented");
+		Assert.assertEquals(2.0, calc.sub(5, 3), 0.0);
+		logger.info("Test 'Sub' erfolgreich!");
 	}
 
 	@Test
-	public void testDiv1() {
-		Assert.assertEquals(20, Calculator.div(12, 0), 0);
-
-	}
-
-	@Test
-	public void testDiv2() {
-		try {
-			Calculator.div(12, 0);
-		} catch (Exception e) {
-			e.printStackTrace();
-			Assert.assertEquals("ArithmeticException", e.getClass().getSimpleName());
-//			Assert.assertEquals(20, Calculator.div(12, 0), 0);
-		}
-
+	public void testDiv() {
+		Assert.assertEquals(2, calc.div(6, 3), 0.0);
+		logger.info("Test 'Div' erfolgreich!");
 	}
 
 	@Test
 	public void testMulti() {
-		fail("Not yet implemented");
+		Assert.assertEquals(15,calc.multi(3, 5),0);
+		logger.info("Test 'Multi' erfolgreich!");
+	}
+
+	@Test
+	public void testSqrt(){
+		Assert.assertEquals(7,calc.sqrt(49),0);
+		logger.info("Test 'Sqrt' erfolgreich!");
+	}
+
+	@Test
+	public void testPow(){
+		Assert.assertEquals(4,calc.pow(2,2),0);
+		logger.info("Test 'Pow' erfolgreich!");
+	}
+
+	@Test
+	public void testMod(){
+		Assert.assertEquals(2, calc.mod(10, 4), 0);
+		logger.info("Test 'Mod' erfolgreich!");
 	}
 
 }
